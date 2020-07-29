e2d452876248dde92d8199370e5a8b06691650c7
Tristan Pacba <tpacba@live.com>
2020-07-28 21:38:50 -0700

    added actions to the choose buttons

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
 var startButton = document.querySelector("#welcome-button");
 var welcomePage = document.querySelector("#welcome-page");
-var questionPage = document.querySelector("#questions");
+var questionPage = document.querySelector("#question-page");
+var questionHead = document.querySelector("#question-header");
+var answers = document.querySelector("#multiple-choice");
+var points = document.querySelector("#points");
 
+var correct = 0;
 
-startButton.addEventListener("click", start);
+var listQuestions = {
+    q0 : "Question 1: Are cats dogs?",
+    q1 : "Question 2: Are dogs cats?",
+    q2 : "Question 3: Are cats cats?",
+    q3 : "Question 4: Are dogs dogs?"
+};
+
+var listAnswers = ["Yes", "No", "Maybe"];
 
 function start() {
     startTimer();
-    hideWelcome();
-    showQuestions();
-}
-
-function hideWelcome() {
     welcomePage.style.display = "none";
-}
-
-function showQuestions() {
     questionPage.style.display = "block";
+    q0();
+};
+
+function q0() {
+    questionHead.textContent = listQuestions.q0;
+    points.textContent = correct;
+    for (var i = 0; i < listAnswers.length; i++) {
+        var list = document.createElement("button");
+        list.textContent = listAnswers[i];
+        list.setAttribute("data-index", i);
+        answers.appendChild(list);
+    }
+    
+    answers.children[0].addEventListener("click", function() {
+        timer -= 5;
+        alert("wrong! You lose 5 seconds.");
+    })
+    answers.children[1].addEventListener("click", function() {
+        correct++;
+        alert("correct!");
+    })
+    answers.children[2].addEventListener("click", function() {
+        timer -= 5;
+        alert("wrong! You lose 5 seconds.");
+    })
 }
 
+
+
+
+
+var timer = 100;
 
 function startTimer() {
-    var timer = 10;
-setInterval(function() {
+    
+    setInterval(function() {
     if (timer <= 0) {
         return;
     } else {
         timer--;
         document.getElementById("timer").textContent = timer;
-    }
-}, 1000)
-}
+    };
+}, 1000);
+};
 
+startButton.addEventListener("click", start);
