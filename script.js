03926f2fe2d451298f1ca659817e3bfb59141776
Tristan Pacba <tpacba@live.com>
2020-07-29 16:47:09 -0700

    trying out with boostrap, storing questions and answers into an object, hoping it'll work

--- script.js
+++ script.js
@@ -1,68 +1,99 @@
-var startButton = document.querySelector("#welcome-button");
 var welcomePage = document.querySelector("#welcome-page");
 var questionPage = document.querySelector("#question-page");
-var questionHead = document.querySelector("#question-header");
-var answers = document.querySelector("#multiple-choice");
-var points = document.querySelector("#points");
+var startButton = document.querySelector("#start-button");
+var questionHeader = document.querySelector("#question-header");
+var multipleChoice = document.querySelector("#multiple-choice");
 
-var correct = 0;
-
-var listQuestions = {
-    q0 : "Question 1: Are cats dogs?",
-    q1 : "Question 2: Are dogs cats?",
-    q2 : "Question 3: Are cats cats?",
-    q3 : "Question 4: Are dogs dogs?"
-};
-
-var listAnswers = ["Yes", "No", "Maybe"];
+startButton.addEventListener("click", start);
 
 function start() {
-    startTimer();
     welcomePage.style.display = "none";
     questionPage.style.display = "block";
-    q0();
-};
+    q1();
+}
 
-function q0() {
-    questionHead.textContent = listQuestions.q0;
-    points.textContent = correct;
-    for (var i = 0; i < listAnswers.length; i++) {
-        var list = document.createElement("button");
-        list.textContent = listAnswers[i];
-        list.setAttribute("data-index", i);
-        answers.appendChild(list);
-    }
+var question1 = {
+    q: "What does HTML stand for?",
+    a1: "Hyper Text Markup Language",
+    a2: "Hyperlinks and Text Markup Language",
+    a3: "Home Tool Markup Language"
+}
+
+function q1() {
+    questionHeader.innerHTML = question1.q;
+    var a1 = document.createElement("button");
+    var a2 = document.createElement("button");
+    var a3 = document.createElement("button");
+
     
-    answers.children[0].addEventListener("click", function() {
-        timer -= 5;
-        alert("wrong! You lose 5 seconds.");
-    })
-    answers.children[1].addEventListener("click", function() {
-        correct++;
-        alert("correct!");
-    })
-    answers.children[2].addEventListener("click", function() {
-        timer -= 5;
-        alert("wrong! You lose 5 seconds.");
-    })
 }
 
 
+// var startButton = document.querySelector("#welcome-button");
+// var welcomePage = document.querySelector("#welcome-page");
+// var questionPage = document.querySelector("#question-page");
+// var questionHead = document.querySelector("#question-header");
+// var answers = document.querySelector("#multiple-choice");
+// var points = document.querySelector("#points");
 
+// var correct = 0;
 
+// var listQuestions = {
+//     q0 : "Question 1: Are cats dogs?",
+//     q1 : "Question 2: Are dogs cats?",
+//     q2 : "Question 3: Are cats cats?",
+//     q3 : "Question 4: Are dogs dogs?"
+// };
 
-var timer = 100;
+// var listAnswers = ["Yes", "No", "Maybe"];
 
-function startTimer() {
+// function start() {
+//     startTimer();
+//     welcomePage.style.display = "none";
+//     questionPage.style.display = "block";
+//     q0();
+// };
+
+// function q0() {
+//     questionHead.textContent = listQuestions.q0;
+//     points.textContent = correct;
+//     for (var i = 0; i < listAnswers.length; i++) {
+//         var list = document.createElement("button");
+//         list.textContent = listAnswers[i];
+//         list.setAttribute("data-index", i);
+//         answers.appendChild(list);
+//     }
     
-    setInterval(function() {
-    if (timer <= 0) {
-        return;
-    } else {
-        timer--;
-        document.getElementById("timer").textContent = timer;
-    };
-}, 1000);
-};
+//     answers.children[0].addEventListener("click", function() {
+//         timer -= 5;
+//         alert("wrong! You lose 5 seconds.");
+//     })
+//     answers.children[1].addEventListener("click", function() {
+//         correct++;
+//         alert("correct!");
+//     })
+//     answers.children[2].addEventListener("click", function() {
+//         timer -= 5;
+//         alert("wrong! You lose 5 seconds.");
+//     })
+// }
 
-startButton.addEventListener("click", start);
+
+
+
+
+// var timer = 100;
+
+// function startTimer() {
+    
+//     setInterval(function() {
+//     if (timer <= 0) {
+//         return;
+//     } else {
+//         timer--;
+//         document.getElementById("timer").textContent = timer;
+//     };
+// }, 1000);
+// };
+
+// startButton.addEventListener("click", start);
